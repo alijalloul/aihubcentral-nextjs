cedbf57bd899ef632bb3c6fef00fe50a7f45acac
unknown <ali.z.jalloul@gmail.com>
2023-05-24 18:35:48 +0300

    added an env file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        OPENAI_API_KEY: "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",
-        MONGODB_URL: "mongodb+srv://reactjsdatabase:12345@cluster0.1djt5oa.mongodb.net/aihubcentral?retryWrites=true&w=majority"
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
