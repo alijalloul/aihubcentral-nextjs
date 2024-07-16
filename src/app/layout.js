2368666a24d36bd26712f7052343a520ad41b2e3
unknown <ali.z.jalloul@gmail.com>
2024-07-16 22:25:18 +0300

    added the google analytics scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,39 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 import Header from "../components/Header/Header.js";
-import Footer from "../components/Footer/Footer.js"
-import MyHead from "../components/MyHead/MyHead.js"
+import Footer from "../components/Footer/Footer.js";
 
-import Providers from './GlobalRedux/provider';
+import Providers from "./GlobalRedux/provider";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <MyHead />
+      {/* <MyHead /> */}
+      <head>
+        <Script
+          strategy="lazyload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-4Q5VG2FDRM"
+        ></Script>
+        <Script id="" strategy="lazyOnload">
+          {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4Q5VG2FDRM');`}
+        </Script>
+      </head>
+
       <body className={inter.className}>
-        < Providers>
+        <Providers>
           <Header />
           {children}
           <Footer />
         </Providers>
       </body>
     </html>
-  )
+  );
 }
