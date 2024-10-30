969acca584bcafe1e7491c0ecd39be2ae0f041ab
alijalloul <ali.z.jalloul@gmail.com>
2024-10-31 00:08:01 +0200

    updated the contact me page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -9,25 +8,46 @@ const Footer = () => {
       <div className="container mx-auto px-6 py-10">
         <div className="md:flex md:items-center md:justify-between">
           <div className="mb-10 md:w-1/2 md:mb-0 md:h-full md:mr-5">
-            <h3 className="text-2xl font-bold text-white mb-2">About AI Hub Central</h3>
-            <p className="text-gray-400">AI Hub Central is a platform where multiple AI technologies are merged into a single simple to use website.</p>
+            <h3 className="text-2xl font-bold text-white mb-2">
+              About AI Hub Central
+            </h3>
+            <p className="text-gray-400">
+              AI Hub Central is a platform where multiple AI technologies are
+              merged into a single simple to use website.
+            </p>
           </div>
           <div className="md:w-1/2 md:h-full">
             <h3 className="text-2xl font-bold text-white mb-2">Contact Us</h3>
             <ul className="text-gray-400 break-words">
-              <li>Email: business.ali.z.jalloul@gmail.com</li>
-              <li><Link href="https://github.com/alijalloul" target='_blank' rel="noreferrer">Github: @alijalloul</Link></li>
-              <li><Link href="https://twitter.com/Jack_OA_Trade" target='_blank' rel="noreferrer">Twitter: @Jack_OA_Trade</Link></li>
-              <li><Link href="https://www.facebook.com/profile.php?id=100092434484333" target='_blank' rel="noreferrer">Facebook: Ali Jalloul</Link></li>
-              <li><Link href="https://www.youtube.com/@JackTrade-om3pv" target='_blank' rel="noreferrer">YouTube: @JackTrade</Link></li>
+              <li>Email: ali.z.jalloul@gmail.com</li>
+              <li>
+                <Link
+                  href="https://github.com/alijalloul"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github: @alijalloul
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.linkedin.com/in/ali-jalloul-370092275/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  LinkedIn: @alijalloul
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
         <hr className="my-12 border-gray-600" />
-        <p className="text-center text-gray-400 text-sm">&copy; 2023 AI Hub Central. All rights reserved.</p>
+        <p className="text-center text-gray-400 text-sm">
+          &copy; 2023 AI Hub Central. All rights reserved.
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
